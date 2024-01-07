cfe48683bf74a1c7765af9c72cb9c670b2300e48
mGunawardhana <mrgunawardhana27368@gmail.com>
2024-01-07 18:11:12 +0530

    making functiuon as industry standerds

--- routes/route.py
+++ routes/route.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException,status
 from models.todos import Todo
 from config.database import collection_name
 from schema.schemas import list_serial
@@ -6,11 +6,19 @@ from bson import ObjectId
 
 router = APIRouter()
 
-@router.get("/todos")
-async def get_todos():
-    todos = list_serial(collection_name.find())
-    return todos
 
+@router.get("/todos", response_model=dict)
+async def get_todos():
+    try:
+        todos = list_serial(collection_name.find())
+        return {"data": todos, "message": "Successfully fetched todos", "status_code": 200}
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Failed to fetch todos: {str(e)}",
+        )
+    
+    
 @router.post("/todos")
 async def create_todos(todo: Todo):
     collection_name.insert_one(dict(todo))
